4f5b44d75ae1762a3ff9ca124688581416e4dee0
Carlos Veliz <velizjcarlos@gmail.com>
2020-07-07 14:15:57 -0700

    initial commit

---
+++ WindowsApp1/Form1.Designer.vb
@@ -0,0 +1,321 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.Button8 = New System.Windows.Forms.Button()
+        Me.Button9 = New System.Windows.Forms.Button()
+        Me.Button10 = New System.Windows.Forms.Button()
+        Me.Button11 = New System.Windows.Forms.Button()
+        Me.Button12 = New System.Windows.Forms.Button()
+        Me.Button13 = New System.Windows.Forms.Button()
+        Me.Button14 = New System.Windows.Forms.Button()
+        Me.Button15 = New System.Windows.Forms.Button()
+        Me.Button16 = New System.Windows.Forms.Button()
+        Me.timer = New System.Windows.Forms.Timer(Me.components)
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.lblScore = New System.Windows.Forms.Label()
+        Me.btnScores = New System.Windows.Forms.Button()
+        Me.btnNew = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.DarkTurquoise
+        Me.Button1.Location = New System.Drawing.Point(102, 118)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(61, 73)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "X"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'Button2
+        '
+        Me.Button2.BackColor = System.Drawing.Color.DarkTurquoise
+        Me.Button2.Location = New System.Drawing.Point(166, 118)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(61, 73)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "X"
+        Me.Button2.UseVisualStyleBackColor = False
+        '
+        'Button3
+        '
+        Me.Button3.BackColor = System.Drawing.Color.DarkTurquoise
+        Me.Button3.Location = New System.Drawing.Point(230, 118)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(61, 73)
+        Me.Button3.TabIndex = 2
+        Me.Button3.Text = "X"
+        Me.Button3.UseVisualStyleBackColor = False
+        '
+        'Button4
+        '
+        Me.Button4.BackColor = System.Drawing.Color.DarkTurquoise
+        Me.Button4.Location = New System.Drawing.Point(294, 118)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(61, 73)
+        Me.Button4.TabIndex = 3
+        Me.Button4.Text = "X"
+        Me.Button4.UseVisualStyleBackColor = False
+        '
+        'Button5
+        '
+        Me.Button5.BackColor = System.Drawing.Color.DarkTurquoise
+        Me.Button5.Location = New System.Drawing.Point(102, 197)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(61, 73)
+        Me.Button5.TabIndex = 7
+        Me.Button5.Text = "X"
+        Me.Button5.UseVisualStyleBackColor = False
+        '
+        'Button6
+        '
+        Me.Button6.BackColor = System.Drawing.Color.DarkTurquoise
+        Me.Button6.Location = New System.Drawing.Point(166, 197)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(61, 73)
+        Me.Button6.TabIndex = 6
+        Me.Button6.Text = "X"
+        Me.Button6.UseVisualStyleBackColor = False
+        '
+        'Button7
+        '
+        Me.Button7.BackColor = System.Drawing.Color.DarkTurquoise
+        Me.Button7.Location = New System.Drawing.Point(230, 197)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(61, 73)
+        Me.Button7.TabIndex = 5
+        Me.Button7.Text = "X"
+        Me.Button7.UseVisualStyleBackColor = False
+        '
+        'Button8
+        '
+        Me.Button8.BackColor = System.Drawing.Color.DarkTurquoise
+        Me.Button8.Location = New System.Drawing.Point(294, 197)
+        Me.Button8.Name = "Button8"
+        Me.Button8.Size = New System.Drawing.Size(61, 73)
+        Me.Button8.TabIndex = 4
+        Me.Button8.Text = "X"
+        Me.Button8.UseVisualStyleBackColor = False
+        '
+        'Button9
+        '
+        Me.Button9.BackColor = System.Drawing.Color.DarkTurquoise
+        Me.Button9.Location = New System.Drawing.Point(102, 276)
+        Me.Button9.Name = "Button9"
+        Me.Button9.Size = New System.Drawing.Size(61, 73)
+        Me.Button9.TabIndex = 15
+        Me.Button9.Text = "X"
+        Me.Button9.UseVisualStyleBackColor = False
+        '
+        'Button10
+        '
+        Me.Button10.BackColor = System.Drawing.Color.DarkTurquoise
+        Me.Button10.Location = New System.Drawing.Point(166, 276)
+        Me.Button10.Name = "Button10"
+        Me.Button10.Size = New System.Drawing.Size(61, 73)
+        Me.Button10.TabIndex = 14
+        Me.Button10.Text = "X"
+        Me.Button10.UseVisualStyleBackColor = False
+        '
+        'Button11
+        '
+        Me.Button11.BackColor = System.Drawing.Color.DarkTurquoise
+        Me.Button11.Location = New System.Drawing.Point(230, 276)
+        Me.Button11.Name = "Button11"
+        Me.Button11.Size = New System.Drawing.Size(61, 73)
+        Me.Button11.TabIndex = 13
+        Me.Button11.Text = "X"
+        Me.Button11.UseVisualStyleBackColor = False
+        '
+        'Button12
+        '
+        Me.Button12.BackColor = System.Drawing.Color.DarkTurquoise
+        Me.Button12.Location = New System.Drawing.Point(294, 276)
+        Me.Button12.Name = "Button12"
+        Me.Button12.Size = New System.Drawing.Size(61, 73)
+        Me.Button12.TabIndex = 12
+        Me.Button12.Text = "X"
+        Me.Button12.UseVisualStyleBackColor = False
+        '
+        'Button13
+        '
+        Me.Button13.BackColor = System.Drawing.Color.DarkTurquoise
+        Me.Button13.Location = New System.Drawing.Point(102, 355)
+        Me.Button13.Name = "Button13"
+        Me.Button13.Size = New System.Drawing.Size(61, 73)
+        Me.Button13.TabIndex = 11
+        Me.Button13.Text = "X"
+        Me.Button13.UseVisualStyleBackColor = False
+        '
+        'Button14
+        '
+        Me.Button14.BackColor = System.Drawing.Color.DarkTurquoise
+        Me.Button14.Location = New System.Drawing.Point(166, 355)
+        Me.Button14.Name = "Button14"
+        Me.Button14.Size = New System.Drawing.Size(61, 73)
+        Me.Button14.TabIndex = 10
+        Me.Button14.Text = "X"
+        Me.Button14.UseVisualStyleBackColor = False
+        '
+        'Button15
+        '
+        Me.Button15.BackColor = System.Drawing.Color.DarkTurquoise
+        Me.Button15.Location = New System.Drawing.Point(230, 355)
+        Me.Button15.Name = "Button15"
+        Me.Button15.Size = New System.Drawing.Size(61, 73)
+        Me.Button15.TabIndex = 9
+        Me.Button15.Text = "X"
+        Me.Button15.UseVisualStyleBackColor = False
+        '
+        'Button16
+        '
+        Me.Button16.BackColor = System.Drawing.Color.DarkTurquoise
+        Me.Button16.Location = New System.Drawing.Point(294, 355)
+        Me.Button16.Name = "Button16"
+        Me.Button16.Size = New System.Drawing.Size(61, 73)
+        Me.Button16.TabIndex = 8
+        Me.Button16.Text = "X"
+        Me.Button16.UseVisualStyleBackColor = False
+        '
+        'timer
+        '
+        Me.timer.Interval = 1500
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Forte", 45.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.Label1.Location = New System.Drawing.Point(102, 24)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(253, 66)
+        Me.Label1.TabIndex = 16
+        Me.Label1.Text = "Memory"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Lucida Calligraphy", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(11, 455)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(286, 17)
+        Me.Label2.TabIndex = 17
+        Me.Label2.Text = "Goal: Keep your score as low as possible."
+        '
+        'lblScore
+        '
+        Me.lblScore.AutoSize = True
+        Me.lblScore.Font = New System.Drawing.Font("Lucida Calligraphy", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblScore.Location = New System.Drawing.Point(11, 502)
+        Me.lblScore.Name = "lblScore"
+        Me.lblScore.Size = New System.Drawing.Size(49, 17)
+        Me.lblScore.TabIndex = 18
+        Me.lblScore.Text = "Score:"
+        '
+        'btnScores
+        '
+        Me.btnScores.BackColor = System.Drawing.Color.LightCyan
+        Me.btnScores.Location = New System.Drawing.Point(361, 470)
+        Me.btnScores.Name = "btnScores"
+        Me.btnScores.Size = New System.Drawing.Size(75, 23)
+        Me.btnScores.TabIndex = 19
+        Me.btnScores.Text = "High Scores"
+        Me.btnScores.UseVisualStyleBackColor = False
+        '
+        'btnNew
+        '
+        Me.btnNew.BackColor = System.Drawing.Color.LightCyan
+        Me.btnNew.Location = New System.Drawing.Point(361, 499)
+        Me.btnNew.Name = "btnNew"
+        Me.btnNew.Size = New System.Drawing.Size(75, 23)
+        Me.btnNew.TabIndex = 20
+        Me.btnNew.Text = "New Game"
+        Me.btnNew.UseVisualStyleBackColor = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.SteelBlue
+        Me.ClientSize = New System.Drawing.Size(459, 556)
+        Me.Controls.Add(Me.btnNew)
+        Me.Controls.Add(Me.btnScores)
+        Me.Controls.Add(Me.lblScore)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button9)
+        Me.Controls.Add(Me.Button10)
+        Me.Controls.Add(Me.Button11)
+        Me.Controls.Add(Me.Button12)
+        Me.Controls.Add(Me.Button13)
+        Me.Controls.Add(Me.Button14)
+        Me.Controls.Add(Me.Button15)
+        Me.Controls.Add(Me.Button16)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button7)
+        Me.Controls.Add(Me.Button8)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Memory"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button7 As Button
+    Friend WithEvents Button8 As Button
+    Friend WithEvents Button9 As Button
+    Friend WithEvents Button10 As Button
+    Friend WithEvents Button11 As Button
+    Friend WithEvents Button12 As Button
+    Friend WithEvents Button13 As Button
+    Friend WithEvents Button14 As Button
+    Friend WithEvents Button15 As Button
+    Friend WithEvents Button16 As Button
+    Friend WithEvents timer As Timer
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents lblScore As Label
+    Friend WithEvents btnScores As Button
+    Friend WithEvents btnNew As Button
+End Class
---
+++ WindowsApp1/Form1.vb
@@ -0,0 +1,325 @@
+﻿Option Explicit On
+Option Infer Off
+Option Strict On
+
+Public Class Form1
+    Structure gameCard
+        Public button As Button
+        Public value As Integer
+    End Structure
+
+    Dim card1 As gameCard
+    Dim card2 As gameCard
+    Dim card3 As gameCard
+    Dim card4 As gameCard
+    Dim card5 As gameCard
+    Dim card6 As gameCard
+    Dim card7 As gameCard
+    Dim card8 As gameCard
+    Dim card9 As gameCard
+    Dim card10 As gameCard
+    Dim card11 As gameCard
+    Dim card12 As gameCard
+    Dim card13 As gameCard
+    Dim card14 As gameCard
+    Dim card15 As gameCard
+    Dim card16 As gameCard
+    Dim cardArray(15) As gameCard
+    Dim randomArray(1, 7) As Integer
+    Dim hScores(9) As String
+
+    Dim selectionCount As Integer
+    Dim activeCardOne As gameCard
+    Dim activeCardTwo As gameCard
+    Dim score As Integer
+    Dim numMatches As Integer
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        initializeGame()
+    End Sub
+
+    Sub initializeGame()
+        card1.button = Button1
+        cardArray(0) = card1
+        card2.button = Button2
+        cardArray(1) = card2
+        card3.button = Button3
+        cardArray(2) = card3
+        card4.button = Button4
+        cardArray(3) = card4
+        card5.button = Button5
+        cardArray(4) = card5
+        card6.button = Button6
+        cardArray(5) = card6
+        card7.button = Button7
+        cardArray(6) = card7
+        card8.button = Button8
+        cardArray(7) = card8
+        card9.button = Button9
+        cardArray(8) = card9
+        card10.button = Button10
+        cardArray(9) = card10
+        card11.button = Button11
+        cardArray(10) = card11
+        card12.button = Button12
+        cardArray(11) = card12
+        card13.button = Button13
+        cardArray(12) = card13
+        card14.button = Button14
+        cardArray(13) = card14
+        card15.button = Button15
+        cardArray(14) = card15
+        card16.button = Button16
+        cardArray(15) = card16
+
+        Dim rand As New Random
+        selectionCount = 0
+        score = 0
+        lblScore.Text = "Score: "
+        numMatches = 0
+
+        For i As Integer = 0 To 7
+            Dim reset As Boolean = False
+            Dim num As Integer = rand.Next(1, 101)
+
+            For j As Integer = 0 To 7
+                If randomArray(0, j) = num Then
+                    i -= 1
+                    reset = True
+                    Exit For
+                End If
+            Next
+            If reset <> True Then
+                randomArray(0, i) = num
+                randomArray(1, i) = num
+            Else
+                System.Diagnostics.Debug.WriteLine("duplicate")
+            End If
+            System.Diagnostics.Debug.WriteLine(num.ToString)
+        Next
+        randomizeCards(rand)
+
+        For i As Integer = 0 To 15
+            cardArray(i).button.BackColor = Color.DarkTurquoise
+            cardArray(i).button.Text = "X"
+        Next
+    End Sub
+
+    Sub cardSelection(ByVal cardnum As Integer)
+        If cardArray(cardnum).button.BackColor = Color.DarkTurquoise Then
+            score += 1
+            lblScore.Text = "Score: " & score.ToString
+            selectionCount += 1
+            cardArray(cardnum).button.BackColor = Color.LimeGreen
+            cardArray(cardnum).button.Text = cardArray(cardnum).value.ToString
+
+            If (selectionCount = 2) Then
+                Button1.Enabled = False
+                Button2.Enabled = False
+                Button3.Enabled = False
+                Button4.Enabled = False
+                Button5.Enabled = False
+                Button6.Enabled = False
+                Button7.Enabled = False
+                Button8.Enabled = False
+                Button9.Enabled = False
+                Button10.Enabled = False
+                Button11.Enabled = False
+                Button12.Enabled = False
+                Button13.Enabled = False
+                Button14.Enabled = False
+                Button15.Enabled = False
+                Button16.Enabled = False
+
+                activeCardTwo = cardArray(cardnum)
+                If (activeCardOne.button.Text = activeCardTwo.button.Text) Then
+                    numMatches += 1
+                    MessageBox.Show("You got a Match!")
+                    activeCardOne.button.BackColor = Color.Gray
+                    activeCardTwo.button.BackColor = Color.Gray
+
+                    activeCardOne = Nothing
+                    activeCardTwo = Nothing
+                End If
+                selectionCount = 0
+                timer.Enabled = True
+                If (numMatches = 8) Then
+                    updateScoresArrayFromFile()
+                    Dim tenthHeighest As Integer
+                    Integer.TryParse(hScores(9).Substring(hScores(9).Length - 2, 2), tenthHeighest)
+                    If score < tenthHeighest Then
+                        MessageBox.Show("Congratulations! you placed in the top ten with a score of " & score.ToString)
+                        hScores(9) = score.ToString
+                        record()
+                    Else
+                        MessageBox.Show("Nice Job! you finished with a score of " & score.ToString)
+                    End If
+                    initializeGame()
+                End If
+            Else
+                activeCardOne = cardArray(cardnum)
+            End If
+
+        End If
+    End Sub
+
+    Sub randomizeCards(rand As Random)
+        For cardIndex As Integer = 0 To 15
+            Dim reset As Boolean = False
+            Dim rCol As Integer = rand.Next(0, 7)
+            Dim rRow As Integer = rand.Next(0, 2)
+
+            System.Diagnostics.Debug.WriteLine(cardIndex.ToString)
+
+            cardArray(cardIndex).value = randomArray(rRow, rCol)
+
+            Do Until cardArray(cardIndex).value <> 0
+                If rCol = 7 Then
+                    cardIndex -= 1
+                    reset = True
+                    Exit Do
+                End If
+                rCol += 1
+                cardArray(cardIndex).value = randomArray(rRow, rCol)
+                'System.Diagnostics.Debug.WriteLine("untilloop")
+            Loop
+
+            If reset <> True Then
+                randomArray(rRow, rCol) = 0
+            Else
+                System.Diagnostics.Debug.WriteLine("reset")
+            End If
+
+        Next
+    End Sub
+
+    Sub record()
+        Dim outFile As IO.StreamWriter = IO.File.CreateText("D:\Carlos\My Documents\CoS\COMP9\Project 1\WindowsApp1\WindowsApp1\HighScores.txt")
+        For i As Integer = 9 To 0 Step -1
+            outFile.WriteLine(hScores(i))
+        Next
+        outFile.Close()
+    End Sub
+
+    Sub updateScoresArrayFromFile()
+        Dim inFile As IO.StreamReader
+        If IO.File.Exists("D:\Carlos\My Documents\CoS\COMP9\Project 1\WindowsApp1\WindowsApp1\HighScores.txt") Then
+            inFile = IO.File.OpenText("D:\Carlos\My Documents\CoS\COMP9\Project 1\WindowsApp1\WindowsApp1\HighScores.txt")
+
+            For i As Integer = 0 To 9
+                hScores(i) = inFile.ReadLine()
+            Next
+            inFile.Close()
+            Array.Sort(hScores)
+        Else
+            MessageBox.Show("High Scores File Not Found")
+        End If
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        cardSelection(0)
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        cardSelection(1)
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        cardSelection(2)
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        cardSelection(3)
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        cardSelection(4)
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        cardSelection(5)
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        cardSelection(6)
+    End Sub
+
+    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
+        cardSelection(7)
+    End Sub
+
+    Private Sub Button9_Click(sender As Object, e As EventArgs) Handles Button9.Click
+        cardSelection(8)
+    End Sub
+
+    Private Sub Button10_Click(sender As Object, e As EventArgs) Handles Button10.Click
+        cardSelection(9)
+    End Sub
+
+    Private Sub Button11_Click(sender As Object, e As EventArgs) Handles Button11.Click
+        cardSelection(10)
+    End Sub
+
+    Private Sub Button12_Click(sender As Object, e As EventArgs) Handles Button12.Click
+        cardSelection(11)
+    End Sub
+
+    Private Sub Button13_Click(sender As Object, e As EventArgs) Handles Button13.Click
+        cardSelection(12)
+    End Sub
+
+    Private Sub Button14_Click(sender As Object, e As EventArgs) Handles Button14.Click
+        cardSelection(13)
+    End Sub
+
+    Private Sub Button15_Click(sender As Object, e As EventArgs) Handles Button15.Click
+        cardSelection(14)
+    End Sub
+
+    Private Sub Button16_Click(sender As Object, e As EventArgs) Handles Button16.Click
+        cardSelection(15)
+    End Sub
+
+    Private Sub timer_Tick(sender As Object, e As EventArgs) Handles timer.Tick
+
+        timer.Enabled = False
+        For i As Integer = 0 To 15
+            If cardArray(i).button.BackColor = Color.LimeGreen Then
+                cardArray(i).button.BackColor = Color.DarkTurquoise
+                cardArray(i).button.Text = "X"
+            End If
+        Next
+        Button1.Enabled = True
+        Button2.Enabled = True
+        Button3.Enabled = True
+        Button4.Enabled = True
+        Button5.Enabled = True
+        Button6.Enabled = True
+        Button7.Enabled = True
+        Button8.Enabled = True
+        Button9.Enabled = True
+        Button10.Enabled = True
+        Button11.Enabled = True
+        Button12.Enabled = True
+        Button13.Enabled = True
+        Button14.Enabled = True
+        Button15.Enabled = True
+        Button16.Enabled = True
+    End Sub
+
+    Private Sub btnNew_Click(sender As Object, e As EventArgs) Handles btnNew.Click
+        initializeGame()
+    End Sub
+
+    Private Sub btnScores_Click(sender As Object, e As EventArgs) Handles btnScores.Click
+
+        Dim scoresText As String = ""
+
+        updateScoresArrayFromFile()
+        For j As Integer = 0 To 9
+            scoresText += hScores(j) & ControlChars.NewLine
+        Next
+
+        MessageBox.Show(scoresText)
+    End Sub
+End Class
